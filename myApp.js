da003f03b5408124272714a23b153f32554165d3
Vishal Prasad <30768888+Vishal0Prasad@users.noreply.github.com>
2023-07-19 19:54:53 +0000

    fixed atlas connection issue

--- myApp.js
+++ myApp.js
@@ -11,17 +11,18 @@ mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopolo
   console.log(err);
 });
 
-const createAndSavePerson = (done) => {
-  let personSchema = new mongoose.Schema({
-    name:{
-      type: String,
-      required: true
-    },
-    age: Number,
-    favoriteFoods: [String]
-  });
+let personSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
 
-  Person = mongoose.model("Person",personSchema); 
+Person = mongoose.model("Person",personSchema);
+
+const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
 
